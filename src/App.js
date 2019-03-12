31b956ad186a0a069d3ae2fc24d60b2b228a41aa
Andy CW Wu <andy_cw_wu@wistron.com>
2019-03-12 19:09:25 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, BrowserRouter } from "react-router-dom"
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 const App = ({ location }) => (
   <Switch>
     <Route path='/' location={location} component={HomePage} exact={true} />
+    <Route path='/login' location={location} component={LoginPage} exact={true} />
   </Switch>
 )
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import React, { Component}  from 'react';
 import logo from './logo.svg';
 import queryString from 'query-string'
 
-export default class HomePage extends Component {
+export default class LoginPage extends Component {
 
   componentDidMount() {
     const values = queryString.parse(this.props.location.search)
@@ -20,7 +20,7 @@ export default class HomePage extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Home Page
+            Login Page
           </p>
           <a
             className="App-link"
@@ -30,7 +30,7 @@ export default class HomePage extends Component {
           >
             Learn React
           </a>
-          <a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1554176659&redirect_uri=https://nogerm.github.io/church-backend/login&state=1234&scope=openid%20profile" onClick={this.handleClick.bind(this)}>Sign in with Github</a>;
+          <a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1554176659&redirect_uri=https://nogerm.github.io/church-backend&state=1234&scope=openid%20profile" onClick={this.handleClick.bind(this)}>Sign in with Github</a>;
         </header>
       </div>
     );
