55e49d0c2583202e0875393f9bd9254ee544ad34
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-03-12 17:13:12 +0800

    Updates

---
+++ precache-manifest.9b6f03c309a00dd50df31b88965c4c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/church-backend/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d8b129f1a9a03ef8e0e2",
+    "url": "/church-backend/static/js/runtime~main.77007e15.js"
+  },
+  {
+    "revision": "3aae7fe920a4bb32a9cb",
+    "url": "/church-backend/static/js/main.5c944905.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/church-backend/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "3aae7fe920a4bb32a9cb",
+    "url": "/church-backend/static/css/main.a23f6138.chunk.css"
+  },
+  {
+    "revision": "498312a6ed6484e53f3318b6542d88dd",
+    "url": "/church-backend/index.html"
+  }
+];
