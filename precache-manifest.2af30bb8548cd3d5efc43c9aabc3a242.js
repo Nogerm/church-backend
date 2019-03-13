4d25f78e3fcd2ca3a925f0fa97990c85658a4996
Andy CW Wu <andy_cw_wu@wistron.com>
2019-03-13 12:40:55 +0800

    Updates

--- precache-manifest.2af30bb8548cd3d5efc43c9aabc3a242.js
+++ precache-manifest.2af30bb8548cd3d5efc43c9aabc3a242.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/church-backend/build/static/js/runtime~main.a08a0833.js"
   },
   {
-    "revision": "3b4b9d37fb984247ff53",
-    "url": "/church-backend/build/static/js/main.a1d54bd6.chunk.js"
+    "revision": "ca3e55973353dae45377",
+    "url": "/church-backend/build/static/js/main.5c0a9c5a.chunk.js"
   },
   {
     "revision": "42cd2ca0efc07fd24417",
     "url": "/church-backend/build/static/js/2.22932306.chunk.js"
   },
   {
-    "revision": "3b4b9d37fb984247ff53",
+    "revision": "ca3e55973353dae45377",
     "url": "/church-backend/build/static/css/main.81d5e7d2.chunk.css"
   },
   {
-    "revision": "7f0545d36b441158371001be1fd90642",
+    "revision": "68c74e747c27e57a5cecbadcc25435ae",
     "url": "/church-backend/build/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/church-backend/build/precache-manifest.83bac38da88fa7eade356def1ca234ce.js"
+  "/church-backend/build/precache-manifest.2af30bb8548cd3d5efc43c9aabc3a242.js"
 );
 
 workbox.clientsClaim();
