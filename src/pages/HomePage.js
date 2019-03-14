36a4be99e9a18d3c37b5aafe046bbec9fff44a0a
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-03-14 15:09:52 +0800

    update layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -72,28 +72,33 @@ export default class HomePage extends Component {
     const loginUrl = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1554176659&redirect_uri=https://nogerm.github.io/church-backend&state=1234&scope=openid%20profile";
     return (
       <Grid>
-        <Grid.Column width={3}>
-          <Menu fluid vertical tabular>
-            <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
-            <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
-            <Menu.Item
-              name='companies'
-              active={activeItem === 'companies'}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name='links'
-              active={activeItem === 'links'}
-              onClick={this.handleItemClick}
-            />
-          </Menu>
-        </Grid.Column>
+        <Grid.Row columns={1} style={{background: 'green'}}>
+          <Header as='h1'>This is a loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong herder</Header>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column width={3}>
+            <Menu fluid vertical tabular>
+              <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
+              <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
+              <Menu.Item
+                name='companies'
+                active={activeItem === 'companies'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name='links'
+                active={activeItem === 'links'}
+                onClick={this.handleItemClick}
+              />
+            </Menu>
+          </Grid.Column>
 
-        <Grid.Column stretched width={12}>
-          <Segment>
-            This is an stretched grid column. This segment will always match the tab height
-          </Segment>
-        </Grid.Column>
+          <Grid.Column stretched width={12}>
+            <Segment>
+              This is an stretched grid column. This segment will always match the tab height
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
