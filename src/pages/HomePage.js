d6181a241c2adca46f6f6048964bbbb06c5ad31e
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-03-14 18:15:59 +0800

    add href

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ import queryString from 'query-string';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import './App.css';
-import { Grid, Menu, Image, Header, Container, Segment } from 'semantic-ui-react'
+import { Grid, Menu, Image, Header, Button, Segment } from 'semantic-ui-react'
 
 export default class HomePage extends Component {
 
@@ -53,37 +53,18 @@ export default class HomePage extends Component {
         console.log("[login] error" + error);
       });
     }
-
-
   }
 
-  checkState = () => {
-
-  }
-
-  handleClick() {
-    
-  }
-
-	render() {
-    const { activeItem } = this.state
-    const userName = this.state.userName;
-    const userImageUrl = this.state.userImage;
+  handleLoginClicked = () => {
     const loginUrl = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1554176659&redirect_uri=https://nogerm.github.io/church-backend&state=1234&scope=openid%20profile";
-    return (
-      <Grid>
-        <Grid.Row columns={1} style={{padding: '0px'}}>
-          <Segment raised style={{background: '#9ccc65', margin: '0px', flex:1}}>
-          <Grid>
-            <Grid.Column width={1}>
-              <Image style={{height:'40px'}} src='https://886point.com/wp-content/uploads/2018/07/icon512-2x-600x600.png'/>
-            </Grid.Column>
-            <Grid.Column width={15}>
-              <Header as='H1' style={{color:'white'}}>LINE Console</Header>
-            </Grid.Column>
-            </Grid>
-          </Segment>
-        </Grid.Row>
+    window.location.href = loginUrl;
+  }
+
+  renderBody = () => {
+    const { activeItem } = this.state
+    const hasLoggedIn = this.state.hasLoggedIn;
+    if(hasLoggedIn) {
+      return (
         <Grid.Row columns={2}>
           <Grid.Column width={3}>
             <Menu fluid vertical tabular>
@@ -108,6 +89,36 @@ export default class HomePage extends Component {
             </Segment>
           </Grid.Column>
         </Grid.Row>
+      )
+    } else {
+      return (
+        <Header as='H1' style={{color:'white'}}>請先登入</Header>
+      )
+    }
+  }
+
+	render() {
+    const renderBody  = this.renderBody;
+    const userName = this.state.userName;
+    const userImageUrl = this.state.userImage;
+    return (
+      <Grid>
+        <Grid.Row columns={1} style={{padding: '0px'}}>
+          <Segment raised style={{background: '#9ccc65', margin: '0px', flex:1}}>
+            <Grid columns={3}>
+              <Grid.Column width={1}>
+                <Image style={{height:'40px'}} src='https://886point.com/wp-content/uploads/2018/07/icon512-2x-600x600.png'/>
+              </Grid.Column>
+              <Grid.Column stretched>
+                <Header as='H1' style={{color:'white'}}>LINE Console</Header>
+              </Grid.Column>
+              <Grid.Column width={2}>
+                <Button floated='right' onClick={this.handleLoginClicked}>LINE LOGIN</Button>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Grid.Row>
+        {renderBody()}
       </Grid>
     );
   }
