7fe276361248bb59154fe9975dea2337ae4f0acf
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-03-14 15:02:32 +0800

    active sementic-ui

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,14 +4,14 @@ import queryString from 'query-string';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import './App.css';
-import { Grid, Menu, Header, Container } from 'semantic-ui-react'
+import { Grid, Menu, Header, Container, Segment } from 'semantic-ui-react'
 
 export default class HomePage extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      activeItem: '生日提醒',
+      activeItem: 'bio',
       hasSendRequest: false,
       hasLoggedIn: false,
       userId: "",
@@ -65,83 +65,34 @@ export default class HomePage extends Component {
     
   }
 
-  renderMenu = () => {
-    const { activeItem } = this.state;
-
-    return (
-      <Container>
-        <Header as='h2' style={{ 'margin': '10px', 'color': 'white' }}>Control Center</Header>
-        <Header as='h4' style={{ 'margin': '10px', 'color': 'white' }}>The Backend of Linebot.</Header>
-        <Menu fluid vertical tabular style={{ 'marginTop': '50px' }}>
-          <Menu.Item 
-            name='生日提醒' 
-            active={activeItem === '生日提醒'} 
-            onClick={this.handleItemClick} 
-          />
-          <Menu.Item 
-            name='分享提醒' 
-            active={activeItem === '分享提醒'} 
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='自動回應'
-            active={activeItem === '自動回應'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='週一祝福'
-            active={activeItem === '週一祝福'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='功能開關'
-            active={activeItem === '功能開關'}
-            onClick={this.handleItemClick}
-          />
-        </Menu>
-      </Container>
-    )
-  }
-
-  renderChild = () => {
-    const { activeItem } = this.state;
-
-    if(activeItem === '生日提醒') {
-      return (
-        <p>生日提醒</p>
-      )
-    } else if(activeItem === '分享提醒') {
-      return (
-        <p>分享提醒</p>
-      )
-    } else if(activeItem === '自動回應') {
-      return (
-        <p>自動回應</p>
-      )
-    } else if(activeItem === '週一祝福') {
-      return (
-        <p>週一祝福</p>
-      )
-    } else if(activeItem === '功能開關') {
-      return (
-        <p>功能開關</p>
-      )
-    }
-  }
-
 	render() {
-    const renderChild = this.renderChild;
-    const renderMenu = this.renderMenu;
+    const { activeItem } = this.state
     const userName = this.state.userName;
     const userImageUrl = this.state.userImage;
     const loginUrl = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1554176659&redirect_uri=https://nogerm.github.io/church-backend&state=1234&scope=openid%20profile";
     return (
       <Grid>
-        <Grid.Column width={3} style={{ background: '#4682B4', paddingRight: 0, height: '100vh' }}>
-          {renderMenu()}
+        <Grid.Column width={3}>
+          <Menu fluid vertical tabular>
+            <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
+            <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
+            <Menu.Item
+              name='companies'
+              active={activeItem === 'companies'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='links'
+              active={activeItem === 'links'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
         </Grid.Column>
+
         <Grid.Column stretched width={12}>
-          {renderChild()}
+          <Segment>
+            This is an stretched grid column. This segment will always match the tab height
+          </Segment>
         </Grid.Column>
       </Grid>
     );
