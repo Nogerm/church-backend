1227ff2d6be7e20a91a35ae77e6e55c9825826b4
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-03-14 18:47:17 +0800

    display user info after login

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React, { Component}  from 'react';
-import logo from './logo.svg';
 import queryString from 'query-string';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
@@ -16,7 +15,7 @@ export default class HomePage extends Component {
       hasLoggedIn: false,
       userId: "",
       userName: "",
-      userImage: ""
+      userImageUrl: ""
     };
   }
 
@@ -100,19 +99,25 @@ export default class HomePage extends Component {
 	render() {
     const renderBody  = this.renderBody;
     const userName = this.state.userName;
-    const userImageUrl = this.state.userImage;
+    const userImageUrl = this.state.userImageUrl;
     return (
       <Grid>
         <Grid.Row columns={1} style={{padding: '0px'}}>
           <Segment raised style={{background: '#9ccc65', margin: '0px', flex:1}}>
-            <Grid columns={3}>
-              <Grid.Column width={1}>
+            <Grid columns={5}>
+              <Grid.Column>
                 <Image style={{height:'40px'}} src='https://886point.com/wp-content/uploads/2018/07/icon512-2x-600x600.png'/>
               </Grid.Column>
-              <Grid.Column stretched>
+              <Grid.Column style={{flex:1}}>
                 <Header as='H1' style={{color:'white'}}>LINE Console</Header>
               </Grid.Column>
-              <Grid.Column width={2}>
+              <Grid.Column>
+                <Image avatar src={userImageUrl} style={{width:'40px', height:'40px'}}/>
+              </Grid.Column>
+              <Grid.Column>
+                <Header as='H1' style={{color:'white'}}>{userName}</Header>
+              </Grid.Column>
+              <Grid.Column>
                 <Button floated='right' onClick={this.handleLoginClicked}>LINE LOGIN</Button>
               </Grid.Column>
             </Grid>
