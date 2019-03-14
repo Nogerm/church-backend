449cd048543df5d596c1f7d76f9268ba519dfe55
Andy CW Wu <andy_cw_wu@wistron.com>
2019-03-14 14:20:11 +0800

    use sementic ui react

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,17 +4,14 @@ import queryString from 'query-string';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import './App.css';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
+import { Grid, Menu, Header, Container } from 'semantic-ui-react'
 
 export default class HomePage extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      activeItem: '生日提醒',
       hasSendRequest: false,
       hasLoggedIn: false,
       userId: "",
@@ -68,30 +65,85 @@ export default class HomePage extends Component {
     
   }
 
+  renderMenu = () => {
+    const { activeItem } = this.state;
+
+    return (
+      <Container>
+        <Header as='h2' style={{ 'margin': '10px', 'color': 'white' }}>Control Center</Header>
+        <Header as='h4' style={{ 'margin': '10px', 'color': 'white' }}>The Backend of Linebot.</Header>
+        <Menu fluid vertical tabular style={{ 'marginTop': '50px' }}>
+          <Menu.Item 
+            name='生日提醒' 
+            active={activeItem === '生日提醒'} 
+            onClick={this.handleItemClick} 
+          />
+          <Menu.Item 
+            name='分享提醒' 
+            active={activeItem === '分享提醒'} 
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='自動回應'
+            active={activeItem === '自動回應'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='週一祝福'
+            active={activeItem === '週一祝福'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='功能開關'
+            active={activeItem === '功能開關'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Container>
+    )
+  }
+
+  renderChild = () => {
+    const { activeItem } = this.state;
+
+    if(activeItem === '生日提醒') {
+      return (
+        <p>生日提醒</p>
+      )
+    } else if(activeItem === '分享提醒') {
+      return (
+        <p>分享提醒</p>
+      )
+    } else if(activeItem === '自動回應') {
+      return (
+        <p>自動回應</p>
+      )
+    } else if(activeItem === '週一祝福') {
+      return (
+        <p>週一祝福</p>
+      )
+    } else if(activeItem === '功能開關') {
+      return (
+        <p>功能開關</p>
+      )
+    }
+  }
+
 	render() {
+    const renderChild = this.renderChild;
+    const renderMenu = this.renderMenu;
     const userName = this.state.userName;
     const userImageUrl = this.state.userImage;
     const loginUrl = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1554176659&redirect_uri=https://nogerm.github.io/church-backend&state=1234&scope=openid%20profile";
     return (
-
-			<div>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" color="inherit">
-              Settings
-            </Typography>
-            <Avatar alt="Remy Sharp" src={userImageUrl}/>
-            <p>{userName}</p>
-            <Button variant="contained"  color="inherit" href={loginUrl}>
-              Login with LINE
-            </Button>
-          </Toolbar>
-        </AppBar>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Home Page
-          </p>
-      </div>
+      <Grid>
+        <Grid.Column width={3} style={{ background: '#4682B4', paddingRight: 0, height: '100vh' }}>
+          {renderMenu()}
+        </Grid.Column>
+        <Grid.Column stretched width={12}>
+          {renderChild()}
+        </Grid.Column>
+      </Grid>
     );
   }
 	
