541c0706d4d86ca368762318edbd84471668a650
Andy CW Wu <andy_cw_wu@wistron.com>
2019-03-19 13:48:33 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/church-backend/precache-manifest.af6555b3ac5503ede7fda0d7c61a97b8.js"
+  "/church-backend/precache-manifest.0de33d6b45f222e4f3e0c77ac577bbfc.js"
 );
 
 workbox.clientsClaim();
