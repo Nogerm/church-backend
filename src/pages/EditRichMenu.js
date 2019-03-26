9df79f9b41808bc3bb0a08da036278805f38c93e
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-03-26 18:33:03 +0800

    add update json

--- src/pages/EditRichMenu.js
+++ src/pages/EditRichMenu.js
@@ -3,11 +3,51 @@ import axios from 'axios';
 import { Header, Segment, Divider } from 'semantic-ui-react';
 import JSONInput from 'react-json-editor-ajrm';
 import locale    from 'react-json-editor-ajrm/locale/en';
+import FileBase64 from 'react-file-base64';
+
+const BASE_URL = "https://nogerm-demo-test.herokuapp.com/";
 
 export default class EditRichMenu extends Component {
 
+	constructor(props) {
+    super(props);
+    this.state = {
+			menu: {},
+      file: {}
+    };
+	}
+
+	handleFileChange = (file) => {
+		if(parseInt(file.size) < 1000) {
+      //size OK
+      this.setState({ 
+        file: file
+			});
+			const post_url = BASE_URL + 'create_rich_menu_for_all';
+      const headers = {
+        'content-type': 'application/json'
+      }
+      const data = {
+				menu: this.state.menu,
+        binary: file.base64
+      }
+      axios.post(post_url, data, headers)
+      .then(response => {
+        console.log("[handleFileChange] success");
+        alert("檔案上傳成功！");
+      })
+      .catch(error => {
+        console.log("[handleFileChange] error" + error);
+        alert("檔案上傳失敗，錯誤訊息：" + error);
+      });
+		}
+	}
+
 	handleJSONChange = (e) => {
-    //console.log("event" + JSON.stringify(e));
+		//console.log("event" + JSON.stringify(e));
+		this.setState({
+			menu: e.jsObject
+		});
   }
 
 	render() {
@@ -34,6 +74,7 @@ export default class EditRichMenu extends Component {
             onChange    = { handleJSONChange }
           />
 					<p>3. 選擇圖文選單的背景圖片（必須為 2500x1686 or 2500x843）</p>
+					<FileBase64 multiple={ false } onDone={ this.handleFileChange.bind(this) } />
 				</Segment>
 			</div>
 			
