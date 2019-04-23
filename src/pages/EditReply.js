af7220d7dedab1b13fa13e9dc62c3dc869418bef
Andy CW Wu <andy_cw_wu@wistron.com>
2019-04-23 13:34:16 +0800

    Update edit reply UI

--- src/pages/EditReply.js
+++ src/pages/EditReply.js
@@ -1,6 +1,6 @@
 import React, { Component}  from 'react';
 import packageJson from '../../package.json';
-import { Image, Header, Button, Segment, Dropdown, Label, Divider, Loader } from 'semantic-ui-react'
+import { Image, Header, Button, Segment, Loader, Icon } from 'semantic-ui-react'
 import JSONInput from 'react-json-editor-ajrm';
 import locale    from 'react-json-editor-ajrm/locale/en';
 import FileBase64 from 'react-file-base64';
@@ -17,13 +17,13 @@ export default class EditReply extends Component {
       filePreviewUrl: "",
       fileBaseUrl: "",
       thumbHeight: "",
-      isuploading: false
+      isUploading: false
     };
   }
 
-  onTypeChange = (e, { value }) => {
+  onTypeChange = (event, data) => {
     this.setState({
-      type: value
+      type: data.value
     },()=>{
       console.log(this.state.type);
     })
@@ -35,17 +35,6 @@ export default class EditReply extends Component {
 
   handleJSONChange = (e) => {
     console.log("event" + JSON.stringify(e));
-
-    //auto replace baseURL for imagemap
-    if(this.state.type === 'imagemap' && this.state.fileBaseUrl !== "") {
-      e.jsObject.baseUrl = this.state.fileBaseUrl;
-    }
-
-    //auto replace hero image for flex
-    if(this.state.type === 'flex' && this.state.fileUrl !== "") {
-      e.jsObject.contents.hero.url = this.state.fileUrl;
-    }
-
     this.props.contentCallback(this.props.id, e);
   }
 
@@ -58,9 +47,9 @@ export default class EditReply extends Component {
         file: file,
         fileUrl: "",
         filePreviewUrl: "",
-        isuploading: true
+        isUploading: true
       });
-      if(this.state.type === 'image') {
+      if(this.state.isSimpleIamge) {
         //upload image, get url and preview url
         const post_url = packageJson.server + '/image_msg_upload';
         const data = {
@@ -78,7 +67,7 @@ export default class EditReply extends Component {
           this.setState({ 
             fileUrl: response.data.fileUrl,
             filePreviewUrl: response.data.filePreviewUrl,
-            isuploading: false
+            isUploading: false
           }, () => {
             const imageMsgObj = {
               "jsObject": {
@@ -95,55 +84,9 @@ export default class EditReply extends Component {
         .catch(error => {
           console.log("[sendUpdateRequest] error" + error);
           alert("檔案上傳失敗，錯誤訊息：" + error);
-        });
-      } else if(this.state.type === 'imagemap') {
-        //upload image, get base url
-        const post_url = packageJson.server + '/imagemap_image_upload';
-        const data = {
-          binary: file.base64,
-          folder_name: this.props.path
-        }
-        axios.post(post_url, data, {
-          headers: {
-            'content-type': 'application/json'
-          }
-        })
-        .then(response => {
-          console.log("[sendUpdateRequest] success");
-          alert("檔案上傳成功！");
-          this.setState({ 
-            fileBaseUrl: response.data.file_base_url,
-            thumbHeight: response.data.thumb_height,
-            isuploading: false
+          this.setState({
+            isUploading: false
           });
-        })
-        .catch(error => {
-          console.log("[sendUpdateRequest] error" + error);
-          alert("檔案上傳失敗，錯誤訊息：" + error);
-        });
-      } else if(this.state.type === 'flex') {
-        //upload image, get url
-        const post_url = packageJson.server + '/image_upload';
-        const data = {
-          binary: file.base64,
-          folder_name: this.props.path
-        }
-        axios.post(post_url, data, {
-          headers:{
-            'content-type': 'application/json'
-          }
-        })
-        .then(response => {
-          console.log("[sendUpdateRequest] success");
-          alert("檔案上傳成功！");
-          this.setState({ 
-            fileUrl: response.data.fileUrl,
-            isuploading: false
-          });
-        })
-        .catch(error => {
-          console.log("[sendUpdateRequest] error" + error);
-          alert("檔案上傳失敗，錯誤訊息：" + error);
         });
       }
     } else {
@@ -151,84 +94,38 @@ export default class EditReply extends Component {
     }
   }
 
-  getTemplateFromType = (type) => {
-    if      (type === 'text')     return {"type":"text", "text":"您好！"}
-    else if (type === 'image')    return {"type": "image", "originalContentUrl": "https://example.com/original.jpg", "previewImageUrl": "https://example.com/preview.jpg"}
-    else if (type === 'video')    return {"type": "video", "originalContentUrl": "https://example.com/original.mp4", "previewImageUrl": "https://example.com/preview.jpg"}
-    else if (type === 'audio')    return {"type": "audio", "originalContentUrl": "https://example.com/original.m4a", "duration": 60000}
-    else if (type === 'sticker')  return {"type": "sticker", "packageId": "1", "stickerId": "1"}
-    else if (type === 'imagemap') return {"type":"imagemap", "baseUrl":"https://example.com/", "altText": "This is an imagemap", "actions": ""}
-    else if (type === 'location') return {"type": "location", "title": "my location", "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１", "latitude": 35.65910807942215, "longitude": 139.70372892916203}
-    else if (type === 'confirm')  return {"type":"template", "template":""}
-    else if (type === 'buttons')  return {"type":"template", "template":""}
-    else if (type === 'carousel') return {"type":"template", "template":""}
-    else if (type === 'flex')     return {"type": "flex", "altText": "This is a Flex Message", "contents": {"type": "bubble", "body": {"type": "box", "layout": "horizontal", "contents": [{ "type": "text", "text": "Hello," }, { "type": "text", "text": "World!" }]}}}
-    else return {}
-  }
-
   renderContent = () => {
+    const { type, isUploading } = this.state;
     const handleJSONChange = this.handleJSONChange;
-    const placeholder = this.props.defaultContent.type === this.state.type ? this.props.defaultContent : this.getTemplateFromType(this.state.type);
+    const placeholder = this.props.defaultContent.type === this.state.type ? this.props.defaultContent : {};
 
-    if(this.state.type === "text" || this.state.type === "location" || this.state.type === "confirm") {
-      //Messages don't need image or file upload
+    if(type === 'image') {
+      const imageSrc = this.state.file.hasOwnProperty("base64") ? this.state.file.base64 : this.props.defaultContent.hasOwnProperty("previewImageUrl") ? this.props.defaultContent.previewImageUrl : '';
+      if(imageSrc === '') {
+        //no image file
+        return (
+          <Segment placeholder>
+            <Header icon>
+              <Icon name='picture' />
+              No images are listed for this customer.
+            </Header>
+            <FileBase64 multiple={ false } onDone={ this.handleFileChange.bind(this) } />
+          </Segment>
+        )
+      } else {
+        //has image file
+        return (
+          <Segment placeholder>
+            {!isUploading && <Image src={imageSrc} size='medium'/>}
+            {isUploading  && <Loader/>}
+          </Segment>
+        )
+      }
+    } else {
       return (
-        <div>
-          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>2. 使用 Bot Designer 設計好訊息</p>
-          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>3. 複製貼上 Bot Designer 產生的程式</p>
-          <JSONInput
-            id          = { this.props.id }
-            placeholder = { placeholder }
-            locale      = { locale }
-            height      = '110px'
-            onChange    = { handleJSONChange }
-          />
-        </div>
-      )
-    } else if(this.state.type === "sticker") {
-      return (
-        <div>
-          <a href="https://devdocs.line.me/files/sticker_list.pdf" rel="noopener noreferrer" target="_blank" title="可以用的貼圖列表">可以用的貼圖列表</a>
-          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>2. 使用 Bot Designer 設計好訊息</p>
-          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>3. 複製貼上 Bot Designer 產生的程式</p>
-          <JSONInput
-            id          = { this.props.id }
-            placeholder = { placeholder }
-            locale      = { locale }
-            height      = '130px'
-            onChange    = { handleJSONChange }
-          />
-        </div>
-      )
-    } else if(this.state.type === "image") {
-      const imageSrc = this.state.file.hasOwnProperty("base64") ? this.state.file.base64 : this.props.defaultContent.hasOwnProperty("previewImageUrl") ? this.props.defaultContent.previewImageUrl : "";
-      return (
-        <div>
-          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>2. 選擇要上傳的圖片 (必須小於1Mb)</p>
-          <FileBase64 multiple={ false } onDone={ this.handleFileChange.bind(this) } />
-          <Image src={imageSrc} size='medium'>
-            <Loader active={this.state.isuploading} inline />
-          </Image>
-        </div>
-      )
-    } else if(this.state.type === "video") {
-      return (
-        <div></div>
-      )
-    } else if(this.state.type === "audio") {
-      return (
-        <div></div>
-      )
-    } else if(this.state.type === "imagemap") {
-      const imageSrc = this.state.file.hasOwnProperty("base64") ? this.state.file.base64 : this.props.defaultContent.hasOwnProperty("previewImageUrl") ? this.props.defaultContent.previewImageUrl : "";
-      return (
-        <div>
-          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>2. 選擇要上傳的圖片</p>
-          <FileBase64 multiple={ false } onDone={ this.handleFileChange.bind(this) } />
-          <Image src={imageSrc} size='medium'>
-            <Loader active={this.state.isuploading} inline />
-          </Image>
-          <div style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>3. 複製貼上 Bot Designer 產生的程式，<Label>altText</Label>輸入在不支援的裝置上要顯示的文字</div>
+        <Segment placeholder>
+          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>1. 使用 Bot Designer 設計好訊息</p>
+          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>2. 複製貼上 Bot Designer 產生的程式</p>
           <JSONInput
             id          = { this.props.id }
             placeholder = { placeholder }
@@ -236,52 +133,41 @@ export default class EditReply extends Component {
             height      = '150px'
             onChange    = { handleJSONChange }
           />
-        </div>
-      )
-    } else if(this.state.type === "flex") {
-      const imageSrc = this.state.file.hasOwnProperty("base64") ? this.state.file.base64 : this.props.defaultContent.hasOwnProperty("previewImageUrl") ? this.props.defaultContent.previewImageUrl : "";
-      return (
-        <div>
-          <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>2. 選擇要上傳的圖片(非必要)</p>
-          <FileBase64 multiple={ false } onDone={ this.handleFileChange.bind(this) } />
-          <Image src={imageSrc} size='medium'>
-            <Loader active={this.state.isuploading} inline />
-          </Image>
-          <div style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>3. 複製貼上 Bot Designer 產生的程式，<Label>altText</Label>輸入在不支援的裝置上要顯示的文字</div>
-          <JSONInput
-            id          = { this.props.id }
-            placeholder = { placeholder }
-            locale      = { locale }
-            height      = '250px'
-            onChange    = { handleJSONChange }
-          />
-        </div>
+        </Segment>
       )
     }
   }
 
+  renderMenu = () => {
+    const onTypeChange = this.onTypeChange;
+
+    return (
+      <Segment>
+        <Button icon='talk'    value='text'  style={{background: this.state.type === 'text' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+        <Button icon='picture' value='image' style={{background: this.state.type === 'image' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+        <Button icon='external square alternate' value='flex' style={{background: this.state.type === 'flex' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+        <Button icon='block layout' value='imagemap' style={{background: this.state.type === 'imagemap' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+        <Button icon='video'   value='video' style={{background: this.state.type === 'video' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+        <Button icon='music'   value='audio' style={{background: this.state.type === 'audio' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+        <Button icon='meh'     value='sticker' style={{background: this.state.type === 'sticker' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+        <Button icon='point'   value='location' style={{background: this.state.type === 'location' ? 'lightgray' : 'white'}} onClick={onTypeChange}/>
+      </Segment>
+    )
+  }
+
 	render() {
+    const renderMenu = this.renderMenu;
     const renderContent = this.renderContent;
-    const msgTypeOptions = [
-      { key: 'text',     text: '文字',    value: 'text',  icon: 'font' },
-      { key: 'image',    text: '圖片',    value: 'image',  icon: 'picture' },
-      { key: 'flex',     text: '彈性訊息', value: 'flex',  icon: 'external square alternate' },
-      { key: 'imagemap', text: '影像地圖', value: 'imagemap',  icon: 'block layout' },
-      { key: 'video',    text: '影片',    value: 'video',  icon: 'video' },
-      { key: 'audio',    text: '語音',    value: 'audio',  icon: 'music' },
-      { key: 'sticker',  text: '貼圖',    value: 'sticker',  icon: 'meh' },
-      { key: 'location', text: '位置',    value: 'location',  icon: 'point' },
-    ];
 
     return (
       <Segment raised>
         <Header as="h3" style={{fontFamily: 'Noto Sans TC'}}>訊息#{this.props.idx+1}
           <Button floated='right' style={{color:'white', background:'#d32f2f'}} onClick={this.handleDeleteClicked}>刪除</Button>
         </Header>
-        <Divider/>
-        <p style={{fontFamily: 'Noto Sans TC', marginTop: '8px'}}>1. 選擇要新增的訊息類別</p>
-        <Dropdown placeholder='Select message type' options={msgTypeOptions} selection defaultValue={this.props.defaultContent.type} onChange={this.onTypeChange} style={{ width:"200px", fontFamily: 'Noto Sans TC' }}></Dropdown>
-        {renderContent()}
+        <Segment.Group>
+          {renderMenu()}
+          {renderContent()}
+        </Segment.Group>
       </Segment>
 		)
 	}
