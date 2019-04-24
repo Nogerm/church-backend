07611d033a7fcd19a5e3580fe493b164747e5cb0
Andy CW Wu <andy_cw_wu@wistron.com>
2019-04-24 09:53:07 +0800

    update analytics

--- src/google_config.js
+++ src/google_config.js
@@ -1,6 +1,7 @@
 export default {
   apiKey: "AIzaSyA3vtWAH1U34t9lpYDP6dPctIe9xTNfUM8",
   discoveryDocs: 
-    ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-  spreadsheetId: "1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo"
+    ["https://www.googleapis.com/discovery/v1/apis/analytics/v3/rest"],
+  clientId: "457436541637-fhgcnaa1isef6c66ettqgk0n5762k8ni.apps.googleusercontent.com",
+  scope: "profile"
 };
--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -15,11 +15,18 @@ export default class Analytics extends Component {
 				window.gapi.client
 					.init({
 						apiKey: googleConfig.apiKey,
-						// Your API key will be automatically added to the Discovery Document URLs.
-						discoveryDocs: googleConfig.discoveryDocs
+						discoveryDocs: googleConfig.discoveryDocs,
+						clientId: googleConfig.clientId,
+						scope: googleConfig.scope
 					})
 					.then(() => {
 					// 3. Initialize and make the API request.
+					// Listen for sign-in state changes.
+          window.gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          this.updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+
 				});
 			},
 			onerror: function() {
@@ -34,11 +41,32 @@ export default class Analytics extends Component {
 		});
 	}
 
+	updateSigninStatus = (isSignedIn) => {
+		// When signin status changes, this function is called.
+		// If the signin status is changed to signedIn, we make an API call.
+		if (isSignedIn) {
+			this.makeApiCall();
+		}
+	}
+
+	makeApiCall = () => {
+		// Make an API call to the People API, and print the user's given name.
+		window.gapi.client.people.people.get({
+			'resourceName': 'people/me',
+			'requestMask.includeField': 'person.names'
+		}).then(function(response) {
+			console.log('Hello, ' + response.result.names[0].givenName);
+		}, function(reason) {
+			console.log('Error: ' + reason.result.error.message);
+		});
+	}
+
+
 	render() {
 		return (
 			<div>
 				<Header as="h1" style={{fontFamily: 'Noto Sans TC'}}>{this.props.title}</Header>
-				<p>Analytics</p>
+				<a href="https://analytics.google.com/analytics/web/#/report-home/a138277882w198888686p193440673" rel="noopener noreferrer" target="_blank" title="Google Analytics">Google Analytics</a>
 			</div>
 		)
 	}
